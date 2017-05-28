8364d9773378d6fe5b34837ef98f81ef108a927a
Binit Datta <binitdatta@gmail.com>
2017-05-28 18:59:08 -0500

    Spring Boot Spring Cloud version upgrade

---
+++ README.md
@@ -0,0 +1,7 @@
+# udemy-rsmortgage-config-service-latest
+
+# This project will be used to keep up with the ever changing Spring Boot and Spring Cloud Versions
+
+# Current Spring Cloud Version is used in this project is Dalston.SR1
+
+# Current Spring Boot Version is 1.5.2.RELEASE
---
+++ src/main/java/com/rollingstone/configserver/RSMortgageConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.rollingstone.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@EnableAutoConfiguration
+@EnableConfigServer
+public class RSMortgageConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RSMortgageConfigServerApplication.class, args);
+	}
+
+}
